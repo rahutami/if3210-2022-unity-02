584b5a13bc37d1f2dfc297ca12c0af4d40c87b13
Allief Nuriman <57051116+alliefn@users.noreply.github.com>
2022-04-04 06:13:12 +0700

    Power and Speed minimal sub attribute (min and max). Penampilan atribut di layar akan diprioritaskan nanti.

--- Assets/Scripts/Player/PlayerPower.cs
+++ Assets/Scripts/Player/PlayerPower.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerPower : MonoBehaviour
 {
-    
+    public int startingPower = 100;
+    public int maxPower = 500;
+
+    public int currentPower;
+
+    public Slider powerSlider;
+
+    public Image powerImage;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("The power of revelation");
+        Debug.Log(maxPower);
+        currentPower = startingPower;
+        Debug.Log(startingPower);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Player/PlayerSpeed.cs
+++ Assets/Scripts/Player/PlayerSpeed.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSpeed : MonoBehaviour
 {
+    public int startingSpeed = 100;
+    public int maxSpeed = 500;
+
+    public int currentSpeed;
+
+    public Slider speedSlider;
+
+    public Image speedImage;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Speed");
+        Debug.Log(maxSpeed);
+        currentSpeed = startingSpeed;
+        Debug.Log(startingSpeed);
     }
 
     // Update is called once per frame
