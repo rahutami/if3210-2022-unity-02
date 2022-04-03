9eab7fc5c1f61951a4d2e33b11290739fa1298b3
Allief Nuriman <57051116+alliefn@users.noreply.github.com>
2022-04-04 06:04:56 +0700

    Init Power and Speed

---
+++ Assets/Scripts/Player/PlayerPower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPower : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("The power of revelation");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
