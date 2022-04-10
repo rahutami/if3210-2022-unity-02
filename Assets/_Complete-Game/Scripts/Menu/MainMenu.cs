5eadfe6e1b4ae287d267c42a28e98078695fe0ee
Gayuh Tri Rahutami <13519192@std.stei.itb.ac.id>
2022-04-10 11:32:55 +0700

    add input name + choose mode

--- Assets/_Complete-Game/Scripts/Menu/MainMenu.cs
+++ Assets/_Complete-Game/Scripts/Menu/MainMenu.cs
@@ -1,12 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public InputField playerName;
     // Start is called before the first frame update
     public void QuitGame(){
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void SetZenMode(){
+        GameMode.gameMode = "zen";
+    }
+    
+    public void SetWaveMode(){
+        GameMode.gameMode = "wave";
+    }
+    
+    public void SetName(){
+        GameMode.playerName = playerName.text;
+        Debug.Log(GameMode.playerName);
+        Debug.Log(GameMode.gameMode);
+        SceneManager.LoadScene("_Complete-Game", LoadSceneMode.Single);
     }
 }
