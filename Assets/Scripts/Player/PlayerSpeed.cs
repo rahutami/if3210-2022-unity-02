152d5d67a8467d982326bc4c875068b5dc675faf
Allief Nuriman <57051116+alliefn@users.noreply.github.com>
2022-04-10 10:22:52 +0700

    Speed refactor

--- Assets/Scripts/Player/PlayerSpeed.cs
+++ Assets/Scripts/Player/PlayerSpeed.cs
@@ -2,41 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerSpeed : MonoBehaviour
 {
-    public int startingSpeed = 100;
-    public int maxSpeed = 500;
-
-    public int currentSpeed;
-
+    public static int currentSpeed = 100;
+    public static int maxSpeed = 500;
     public Slider speedSlider;
-
     public Image speedImage;
-
-    // Create method to change currentSpeed
-    public void ChangeSpeed(int newSpeed)
-    {
-        // check if newSpeed is greater than maxSpeed
-        if (newSpeed > maxSpeed)
-        {
-            // set currentSpeed to maxSpeed
-            currentSpeed = maxSpeed;
-        }
-        else
-        {
-            // set currentSpeed to newSpeed
-            currentSpeed = newSpeed;
-        }
-    }
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Speed");
-        Debug.Log(maxSpeed);
-        currentSpeed = startingSpeed;
-        Debug.Log(startingSpeed);
+        Debug.Log("The following value is the current speed: " + currentSpeed);
+        Debug.Log(PlayerSpeed.currentSpeed);
     }
 
     // Update is called once per frame
